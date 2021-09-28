0ee1e614843f86cc2e980ff49823c22b0f6957fa
sebasttian9 <svalenzuela.ortiz@gmail.com>
2021-09-28 16:34:50 -0300

    Primera Entrega

--- ApellidoNombre.js
+++ ApellidoNombre.js
@@ -1,1 +1,58 @@
-console.log('Hellou word');
+
+class Usuario {
+
+    constructor(nombre,apellido,libros,mascotas){
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+        console.log('this:',this);
+
+    }
+
+
+    getFullName = () => {
+
+            return `Nombre Completo: ${this.nombre +' '+ this.apellido}`;
+    }
+
+    addMascota = (nombre) => {
+
+        this.mascotas.push(nombre);
+    }
+
+    countMascotas = () => {
+
+        return this.mascotas.length;
+    }
+
+    addBook = (nombre,autor) => {
+
+        this.libros.push({nombre: nombre,autor: autor});
+
+    }
+
+    getBookNames = () => {
+
+        let nombreLibros = [];
+        this.libros.map(l => {
+           nombreLibros.push(l.nombre); 
+        })
+
+        return nombreLibros; 
+    }
+
+}
+
+libros = [{ nombre: 'Papelucho', autor: 'Marcela Paz'}];
+mascotas = ['Perro'];
+user = new Usuario('Sebastian','Valenzuela',libros,mascotas);
+
+user.addMascota('gato')
+user.addBook('seba','paula');
+console.log(user.countMascotas());
+console.log(user.getFullName());
+// console.log(user);
+console.log(user.getBookNames());
+/// lalalalalala
